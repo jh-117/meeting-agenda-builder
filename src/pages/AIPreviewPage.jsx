5786d3781276e89c4f3c1f726a67ea628767fc27
jh-117 <limjiahuey2004@gmail.com>
2025-11-27 21:56:01 +0800

    Updated AIPreviewPage.jsx

--- src/pages/AIPreviewPage.jsx
+++ src/pages/AIPreviewPage.jsx
@@ -455,7 +455,7 @@ function AIPreviewPage() {
       </div>
 
       <style>{`
-        @keyframes spin {
+        @keyframes spin 
           from { transform: rotate(0deg); }
           to { transform: rotate(360deg); }
         }
